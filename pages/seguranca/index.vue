7bd79a9ecb6dd982b422c43110961d6756f3bb9c
Adjair Costa Farias <adjaircfarias@gmail.com>
2021-04-11 12:14:50 -0300

    pagina seguranca

--- pages/seguranca/index.vue
+++ pages/seguranca/index.vue
@@ -1,87 +1,135 @@
 <template>
   <div>
-    <section id="uma-alternativa">
-      <div class="container mx-auto">
-        <div class="flex-1 gap-8 py-16 hidden lg:block">
-          <img
-            class="mx-auto img-seg"
-            src="~/static/seguranca/uma_alternativa.jpeg"
-            alt="uma alternativa"
-          />
-        </div>
-      </div>
-    </section>
-
     <section id="barreira" class="bg-brand-green">
       <div class="container mx-auto">
-        <div class="flex-1 gap-8 py-16 hidden lg:block">
+        <div class="flex-1 lg:block">
           <img
-            class="mx-auto img-seg"
-            src="~/static/seguranca/barreira_protecao.jpeg"
-            alt="uma alternativa"
+            class="mx-auto transform scale-75"
+            src="~/static/cartaz_1.png"
+            alt="cartaz_1"
           />
         </div>
       </div>
     </section>
 
-    <section id="residuos" class="bg-green-100">
+    <section id="vamos-juntos">
       <div class="container mx-auto">
-        <div class="flex-1 gap-8 py-16 hidden lg:block">
+        <div class="flex-1 py-2 lg:block">
           <img
             class="mx-auto img-seg"
-            src="~/static/seguranca/residuos_barreira.jpeg"
-            alt="uma alternativa"
+            src="~/static/pelo_bem_viver.png"
+            alt="pelo_bem_viver"
+          />
+        </div>
+        <div class="flex-1 py-2 lg:block">
+          <img
+            class="mx-auto img-seg"
+            src="~/static/vamos_juntos.png"
+            alt="vamos_juntos"
           />
         </div>
       </div>
     </section>
 
-    <section id="residuos" class="bg-green-200">
+    <section id="passo-1" class="bg-green-100">
       <div class="container mx-auto">
-        <div class="py-16 lg:py-20">
-          <h3 class="mb-6 text-base text-grey-400">
-            <p>
-              🖇️ Papel: jornal, revista, caderno, livro, papelão, caixa de
-              sapato, caixa de sabão em pó, caixa de leite ou suco, rolo de
-              papel (higiênico, papel alumínio ou de plástico filme),saco de
-              pão, papel propaganda de supermercado, caixa de remédio, caixa de
-              congelados, caixa de pizza (sem estar suja de gordura) boleto de
-              cobrança, propaganda política, panfletos em geral ....
-            </p>
-            <p>
-              🥤 Plástico: garrafa de refri, de óleo, de suco, de vinagre, de
-              água, galão de água, garrafa de água sanitária, embalagem de
-              shampoo, garrafas de álcool em gel e produtos de limpeza, plástico
-              de embalagens de papel higiênico, de pão de forma, de batatinha
-              frita congelada... sacolinhas de supermercado, embalagens de
-              manteiga, margarina, maionese, creamcheese, achocolatados,
-              sorvetes... cadeira de plástico quebrada ou mesa, potes quebrados,
-              canudinhos, brinquedos quebrados, desodorante rollon... vaso de
-              planta de plástico, tampinhas de garrafa em geral, tampinhas de
-              cremes e tampinha de pomadas... ⛓️ Metal : latinhas de cerveja ou
-              refri, lata de conserva em geral, lata de leite em pó ou
-              achocolatado, panela velha, marmitex de alumínio (limpo),
-              desodorante spray e qualquer embalagem aerossol, sprays em geral,
-              lata de tinta vazia
-            </p>
-            <p>
-              🍾 Vidro : garrafas em geral, vidros de perfume, vidros de
-              conserva em geral
-            </p>
-            <p>
-              🚯❌Evitar: Papel: papéis engordurados, caixas de ovo ou papel
-              fotográfico Plástico : caixas de ovo de plástico ou isopor e
-              hortifruti q vem em embalagens de plástico, isopor de alimento,
-              acrílico, embalagens metalizadas de biscoito ou chips, embalagens
-              de alguns iogurtes (como leite fermentados), garrafa pet rosa ou
-              vermelha. Vidro : espelhos quebrados ou vidro temperado quebrado
-            </p>
-            <p>
-              ♻️ Ninguém é obrigado a saber tudo que é reciclável e o que não é
-              , vamos aprendendo... Na dúvida, coloca no reciclável e vamos
-              aprendendo juntos. Qualquer dúvida, tamo aí 🖤
-            </p>
-          </h3>
+        <div class="flex-1 lg:block">
+          <img
+            class="mx-auto img-seg"
+            src="~/static/barreira.png"
+            alt="barreira"
+          />
+        </div>
+        <div class="flex-1 lg:block">
+          <img
+            class="mx-auto img-seg"
+            src="~/static/passo_1.png"
+            alt="passo_1"
+          />
+        </div>
+      </div>
+    </section>
+
+    <section id="passo-2" class="bg-green-200">
+      <div class="container mx-auto">
+        <div class="flex-1 lg:block">
+          <img
+            class="mx-auto img-seg"
+            src="~/static/passo_2.png"
+            alt="passo_2"
+          />
+        </div>
+      </div>
+    </section>
+
+    <section id="passo-3" class="bg-green-300">
+      <div class="container mx-auto">
+        <div class="flex-1 lg:block">
+          <img
+            class="mx-auto img-seg"
+            src="~/static/passo_3.png"
+            alt="passo_3"
+          />
+        </div>
+      </div>
+    </section>
+
+    <section id="passo-4" class="bg-green-400">
+      <div class="container mx-auto">
+        <div class="flex-1 lg:block">
+          <img
+            class="mx-auto img-seg"
+            src="~/static/passo_4.png"
+            alt="passo_4"
+          />
+        </div>
+      </div>
+    </section>
+
+    <section id="passo-5">
+      <div class="container mx-auto">
+        <div class="flex-1 lg:block">
+          <img
+            class="mx-auto img-seg"
+            src="~/static/passo_5.png"
+            alt="passo_5"
+          />
+        </div>
+      </div>
+    </section>
+
+    <section id="passo-6" class="bg-green-100">
+      <div class="container mx-auto">
+        <div class="flex-1 lg:block">
+          <img
+            class="mx-auto img-seg"
+            src="~/static/passo_6.png"
+            alt="passo_6"
+          />
+        </div>
+      </div>
+    </section>
+
+    <section id="passo-7" class="bg-green-200">
+      <div class="container mx-auto">
+        <div class="flex-1 lg:block">
+          <img
+            class="mx-auto img-seg"
+            src="~/static/passo_7.png"
+            alt="passo_7"
+          />
+        </div>
+      </div>
+    </section>
+
+    <section id="como-separar" class="bg-green-300">
+      <div class="container mx-auto">
+        <div class="flex-1 lg:block">
+          <img
+            class="mx-auto img-seg p-3"
+            src="~/static/como_separar.png"
+            alt="como_separar"
+          />
         </div>
       </div>
     </section>
